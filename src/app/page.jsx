90ead645604afe34e8f7652d961a6820b74a3e46
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-07-26 12:38:26 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -40,7 +40,7 @@ export default function Page() {
 
     const Clickitem = (e) => {
         if (targetUrl) {
-         window.location.href="https://cxxx.vercel.app";
+         window.location.href="https://cxxx.vercel.app/home";
         } else {
             console.error('Target URL is not set');
         }
